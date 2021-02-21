42fa2f43a09fb280eb237e004020430fee883cf0
Microsoft <Microsoft@DESKTOP-7NSCBFQ>
2021-02-21 23:06:34 +0300

    deneme

--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -6,7 +6,7 @@ namespace KampIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");   
         }
     }
 }
