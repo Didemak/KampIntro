df8c78bafe7062854e75ce0e4d3448321d79732d
Microsoft <Microsoft@DESKTOP-7NSCBFQ>
2021-02-23 01:54:20 +0300

    Nesne Oluşturma

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+
+        }
+    }
+}
