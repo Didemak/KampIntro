6e8a5572f3b3712c94afc124e288cd5608ddc103
Microsoft <Microsoft@DESKTOP-7NSCBFQ>
2021-02-23 02:31:19 +0300

    İf Else Blokları

--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -6,7 +6,33 @@ namespace KampIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");   
+            bool sistemeGirisYamismi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+
+            if (sistemeGirisYamismi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
         }
     }
 }
