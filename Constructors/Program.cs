da2660a62bb35648d088598d6d2041ab3b18b53d
Microsoft <Microsoft@DESKTOP-7NSCBFQ>
2021-02-24 13:51:32 +0300

    Parametreli Constructor Yazımı

---
+++ Constructors/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, FirstName = "Engin", LastName = "Dimiroğ", City="Ankara" };//1. //customer1.ıd=1 bunlar aynı kullanımdır
+            Customer customer2 = new Customer(2,"Derin","Demiroğ","Ankara"); //Normal parantez görürsniz metot mantığı vardır //2.
+            Console.WriteLine(customer2.FirstName); //23. satırda atama işlemi yaptığım için yazdırdı.
+            Console.WriteLine(customer1.Id);  
+        }
+
+        class Customer
+        {
+            public Customer() //1. //parametresi olmayanlar için oluşturduk. // default constructor: parametresi olmayana denir
+            {
+             
+            }
+            public Customer(int id,string firstName,string lastName,string city) //2. //parametresi olanlar için oluşturduk // sanki bir metotmuş gibi constructor bıraktık. parametre gibi çalışır //newleme yapınca bu blok çalışır
+            {
+                Id = id;                //Constructor da verdiğim parametreleri yazdırmak için önce hepsi için atama işlei yapmalıyım yoksa yazmaz.
+                FirstName = firstName;
+                LastName = lastName;
+                City = city;
+
+            }
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string City { get; set; }
+
+        }
+    }
+
+}
